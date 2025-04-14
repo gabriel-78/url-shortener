7e280ec6bcd66d67eb77e0f26115009bcd3252cb
gabriel-78 <gabriel.78.rodrigues@gmail.com>
2025-04-14 20:17:13 -0300

    Config(EU-004): Config web project

---
+++ web/eslint.config.js
@@ -0,0 +1,57 @@
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+      prettier,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'warn',
+      semi: ['error', 'always'],
+    },
+  },
+  {
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'auto', // ou 'lf' ou 'crlf', dependendo da sua preferência
+        },
+      ],
+    },
+  },
+];
---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import Error404 from "./assets/Error404.svg"
+
+function App() {
+  return (
+    <div>
+      <img alt="" src={Error404}/>
+      <h1 className="text-danger text-xs">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
