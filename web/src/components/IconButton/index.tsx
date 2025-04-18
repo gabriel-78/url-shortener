6d5cd3048e5dade4dc311b792a12794cc1aac90a
gabriel-78 <gabriel.78.rodrigues@gmail.com>
2025-04-18 00:51:41 -0300

    Feat(EU-007): Add default IconButton component

---
+++ web/src/components/IconButton/index.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx';
+import { ComponentProps } from 'react';
+
+function IconButton({ ...props }: ComponentProps<'button'>) {
+  return (
+    <button
+      {...props}
+      className={clsx(
+        'flex w-fit h-fit gap-2 items-center transition-colors disabled:opacity-50 p-2 [&>svg]:size-4 rounded border border-solid border-transparent hover:border-blue-dark bg-gray-200 text-gray-500 text-sm-semibold',
+        props.className,
+      )}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+export default IconButton;
