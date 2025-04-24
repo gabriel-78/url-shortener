bd6f2dff7816b0cf1ea6f678d1552820476a8fef
gabriel-78 <gabriel.78.rodrigues@gmail.com>
2025-04-23 21:28:14 -0300

    Config: Setup env files

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,9 +1,12 @@
+import { env } from '@/env';
 import fastifyCors from '@fastify/cors';
 import fastify from 'fastify';
 
 const server = fastify();
 
 server.register(fastifyCors, { origin: '*' });
+
+console.log(env);
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server runing');
