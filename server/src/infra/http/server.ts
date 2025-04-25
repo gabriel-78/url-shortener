69bedbac4f4fc52dbf85cbf7c261d0b2e555b1c9
gabriel-78 <gabriel.78.rodrigues@gmail.com>
2025-04-24 21:13:34 -0300

    Config: Setup validators

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,8 +1,27 @@
 import { env } from '@/env';
 import fastifyCors from '@fastify/cors';
 import fastify from 'fastify';
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
 
 const server = fastify();
+
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    });
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' });
+});
 
 server.register(fastifyCors, { origin: '*' });
 
